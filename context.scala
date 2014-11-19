b5c9e4b235e5d883ff468af2328e3303b4763169
Emma Holmberg Ohlsson <ada10eho@student.lu.se>
2014-11-19 12:32:39 +0100

    change context

--- context.scala
+++ context.scala
@@ -1,6 +1,8 @@
 Model(
 	Product("TraficOMeter") interactsWith (
 		User("Staten"),
-		User("Slutanvändare"),
-		System("GPS"),
-		System("Kollektivtrafiksystem")))
+		User("Slutanvändare") interactsWith (Product("TraficOMeter"))
+	),
+	System("GPS") interactsWith (Product("TraficOMeter")),
+	System("Kollektivtrafiksystem") interactsWith (Product("TraficOMeter"))
+)
