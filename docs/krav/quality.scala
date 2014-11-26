9d28bb72d5ae22c199d7b30add022b21867db4e0
Emma Holmberg Ohlsson <ada10eho@student.lu.se>
2014-11-26 16:05:15 +0100

    quality goals added from exercise

--- docs/krav/quality.scala
+++ docs/krav/quality.scala
@@ -1,7 +1,11 @@
 Model(
-	Product("Our Product") has (
-		Quality("requestCapacity") has (
-			Spec("The system needs to be able to process at least 20000 update requests per minute")
-		)
-	)
-)
+  Product("Our Product") has (
+    Quality("requestCapacity") has 
+      Spec("The system needs to be able to process at least 20000 update requests per minute"), 
+    Quality("UpdateStartview-Usability") has 
+      Spec("Updating startview shall be possible with one buttonclick per time"), 
+    Quality("Uptime-Reliability") has Spec("The system should have an uptime of 99%"), 
+    Quality("GPS-position-Efficiency") has 
+      Spec("The system should give a GPS-position correct within a 50 m radius"), 
+    Quality("Bytestid-Efficiency") has Spec("The product will not have byten of less than 5 min?????"), 
+    Quality("personal-Security") has Spec("The system will not connect position with person")))
