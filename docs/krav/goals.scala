bea5d91088655881c62da506d0cc71cf0c56dbe0
Johan Mattsson <adi10jma@student.lu.se>
2014-12-01 18:00:04 +0100

    added stakeholder to goals.scala

--- docs/krav/goals.scala
+++ docs/krav/goals.scala
@@ -1,12 +1,18 @@
 Model(
 	Product("Our Product") has (
-		Goal("moreTravel") has (
-			Spec("Increase the use of public transportation by making it a more effortless to find travel connections")
-		),
-		Goal("userTravelStatistics") has (
-			Spec("The system will provide data about the users possible travels that can be analysed to reveal flaws in the local transportation system")
+		Stakeholder("Staten") has (
+			Goal("moreTravel") has (
+				Spec("Increase the use of public transportation by making it a more effortless to find travel connections")
+				),
+			Goal("userTravelStatistics") has (
+				Spec("The system will provide data about the users possible travels that can be analysed to reveal flaws in the local transportation system")
+				)
+			)
+		Stakeholder("User") has (
+			Goal("travelOverview") has (
+				
+				)
+			)
+
 		)
-
-
 	)
-)
