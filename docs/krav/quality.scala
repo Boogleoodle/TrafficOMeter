24093215f0623140047f8df113cd5b533974cb46
Philip Holgersson <philip.holgersson@gmail.com>
2014-12-01 14:26:05 +0100

    blä

--- docs/krav/quality.scala
+++ docs/krav/quality.scala
@@ -4,7 +4,8 @@ Model(
       Spec("The system needs to be able to process at least 20000 update requests per minute"), 
     Quality("UpdateStartview-Usability") has 
       Spec("Updating startview shall be possible with one buttonclick per time"), 
-    Quality("Uptime-Reliability") has Spec("The system should have an uptime of 99%"), 
+    Quality("Uptime-Reliability") has 
+      Spec("The system should have an uptime of 99%"), 
     Quality("GPS-position-Efficiency") has 
       Spec("The system should give a GPS-position correct within a 50 m radius"), 
     Quality("Bytestid-Efficiency") has Spec("The product will not have byten of less than 5 min?????"), 
