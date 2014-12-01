4c770a4a19e8c1b9f7464abfebb6165b9b7488d8
Johan Mattsson <adi10jma@student.lu.se>
2014-12-01 16:12:30 +0100

    lagt till sections i quality.scala

--- docs/krav/quality.scala
+++ docs/krav/quality.scala
@@ -1,12 +1,25 @@
 Model(
   Product("Our Product") has (
-    Quality("requestCapacity") has 
-      Spec("The system needs to be able to process at least 20000 update requests per minute"), 
-    Quality("UpdateStartview-Usability") has 
-      Spec("Updating startview shall be possible with one buttonclick per time"), 
-    Quality("Uptime-Reliability") has 
-      Spec("The system should have an uptime of 99%"), 
-    Quality("GPS-position-Efficiency") has 
+    Section("Usability") has (
+      Quality("UpdateStartview") has 
+      Spec("Updating startview shall be possible with one buttonclick per time")
+      ),
+    Section("Efficiency") has (
+      Quality("GPS-position") has 
       Spec("The system should give a GPS-position correct within a 50 m radius"), 
-    Quality("Bytestid-Efficiency") has Spec("The product will not have byten of less than 5 min?????"), 
-    Quality("personal-Security") has Spec("The system will not connect position with person")))
+      Quality("Bytestid") has 
+      Spec("The product will not have byten of less than 5 min?????"),
+      Quality("requestCapacity") has 
+      Spec("The system needs to be able to process at least 20000 update requests per minute") 
+      ),
+    Section("Reliability") has (
+      Quality("Uptime-Reliability") has 
+      Spec("The system should have an uptime of 99%")
+      ),
+    Section("Security") has (
+      Quality("personal-Security") has Spec("The system will not connect position with person")
+      )
+
+    
+    )
+  )
