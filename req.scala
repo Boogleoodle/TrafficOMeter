b6aa5dd4392684385ee4446789cba56b426e6999
Philip Holgersson <philip.holgersson@gmail.com>
2014-12-01 13:13:33 +0100

    added req.scala

---
+++ req.scala
@@ -0,0 +1,19 @@
+Model (
+	Req("Visibility"),
+	Req("ViewNextOption"),
+	Req("GeographicalLocation"),
+	Req("FastestOption"),
+	Req("SaveLocation"),
+	Req("SpecifyByMap"),
+	Req("SpecifyMeansOfTravel"),
+	Req("SearchDestination"),
+	Req("ChangeNameOnSavedLocation"),
+	Req("RemoveLocation"),
+	Req("MultipleLOcations"),
+	Req("ShowRouteTo FirstStop"),
+	Req("requestCapacity"),
+	Req("Uptime-Reliability"),
+	Req("GPS-position-Efficiancy"),
+	Stakeholder("Users"),
+	Stakeholder("Staten")
+	)
