fcad97f81e87733e136e1c5df8d9ff2b04f34cd0
M4tt3 <mat10mek@student.lu.se>
2014-12-02 14:16:25 +0100

    Added tasks.  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   SakerAttFixa.txt
    	modified:   task1.scala

--- docs/task1.scala
+++ docs/task1.scala
@@ -1,4 +1,58 @@
 Model(
-  Task("AddDestination") has (
-    Why("User wants to have another destination for future trip alternatives"), 
-    Spec("1.Find location, 2.give the place a name, 3.save destination. Variants: 1a) Take current position, 1b) Searched for place does not exist, 1c) Multiple place matches search. Purpose: To add a new destination so that trips to that destination are shown in the future. Trigger: User want another destination to travel to. Frequency: Frequent when downloading app, or moving, otherwise not frequent")))
+	Task("findLocation") has (
+		Spec("Locacte a location to travel to"),
+		Variant("Select current GPS-coordinates as location"),
+		Variant("Desiered location does not exist"),
+		Variant("Multiple matches for location query"),
+		Frequency(5)
+	),
+	Task("nameLocation") has (
+		Spec("Names a location"),
+		Frequency(1)
+	),
+	Task("saveLocation") has (
+		Spec("Save location to device and database"),
+		Frequency(1)
+	),
+	Task("addDestination") has (
+		Spec("User saves a location\nTrigger: User want another destination to travel to"),
+		Task("Find Location"),
+		Task("Name Location"),
+		Task("Save Location"),
+		Why("The user shall not have to search for a destination he/she might visit."),
+		Frequency(3)
+	),
+	Task("toggleTrain") has (
+		Spec("Toggles wheter the or not user wants to go by train."),
+		Frequency(2)
+	),
+	Task("toggleBus") has (
+		Spec("Toggles wheter or not the user wants to go by bus."),
+		Frequency(2)
+	),
+	Task("toggleFerry") has (
+		Spec("Toggles wheter or not the user wants to go by bus."),
+		Frequency(1)
+	),
+	Task("chooseTransportationMode") has (
+		Spec("User specifies what mode of transportation he/she wants to use."),
+		Task("toggleTrain"),
+		Task("toggleBus"),
+		Task("toggleFerry"),
+		Frequency(1)
+	),
+	Task(selectSavedLocation) has (
+		Spec("Select a location from a list of saved locations"),
+		Frequency(1)
+	),
+	Task("deleteSelectedLocation") has (
+		Spec("Removes a selected location"),
+		Frequency(1),
+	),
+	Task("removeDestination") has (
+		Spec("Remove a previously saved location"),
+		Task("SelectSavedLocation"),
+		Task("deleteSelectedLocation"),
+		Frequency(1)
+ 	)
+)
