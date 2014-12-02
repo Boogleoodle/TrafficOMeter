5a037f2c3831997ac2710c1683a04c63a0db9066
Philip Holgersson <philip.holgersson@gmail.com>
2014-12-02 10:29:57 +0100

    Added design.scala that contains all the design requirements

---
+++ docs/krav/design.scala
@@ -0,0 +1,84 @@
+Model(
+	Product("Our Product") has (
+    	Section("Widget") has (
+    		Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+    		Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+  			Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+			Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+		)
+		
+		Section("Applikation") has (
+    		Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+    		Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+  			Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+			Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+		)
+
+		Section("RouteInformation") has (
+    		Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+    		Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+  			Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+			Feature("")
+    			Spec(""),
+    			Why(""),
+    			Example(""),
+    			Satus(ELICITED)
+    		),
+		)
+	)
+)
