b001598ae58ea7f24f2d2281944893425bb47d77
Philip Holgersson <philip.holgersson@gmail.com>
2014-12-03 14:08:45 +0100

    gjort designkrav för det visuella

--- docs/krav/design.scala
+++ docs/krav/design.scala
@@ -4,89 +4,32 @@ Model(
         Section("Visual") has (
 
         	Section("Widget") has (
-        		Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-        		Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-      			Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-    			Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
+        		Feature("Appearance") has (
+        			Spec("The product shall look like the screen pictures named widget?? shown in appendix ??"),
+        			Why("The screen pictures displays the way that the product shall look like"),
+        			Example("As the pictures in appendix ??"),
         			Status(ELICITED)
         		)
     		),
     		
-    		Section("Applikation") has (
-        		Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-        		Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-      			Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-    			Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		)
+    		Section("Application") has (
+        		Feature("Appearance") has (
+                    Spec("The product shall look like the screen pictures named app?? shown in appendix ??"),
+                    Why("The screen pictures displays the way that the product shall look like"),
+                    Example("As the pictures in appendix ??"),
+                    Status(ELICITED)
+                )
     		),
 
     		Section("RouteInformation") has (
-        		Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-        		Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-      			Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		),
-    			Feature("") has (
-        			Spec(""),
-        			Why(""),
-        			Example(""),
-        			Status(ELICITED)
-        		)
+        		Feature("Appearance") has (
+                    Spec("The product shall look like the screen pictures named info?? shown in appendix ??"),
+                    Why("The screen pictures displays the way that the product shall look like"),
+                    Example("As the pictures in appendix ??"),
+                    Status(ELICITED)
+                )
     		)
     	)
-
-		Section("något annat designmässigt") has (
-
-		)
+		
     )
 )
