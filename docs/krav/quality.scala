f265c0250c567165d59c45b9be303264bde3a872
Hannes <adi10hjo@student.lth.se>
2014-12-03 14:15:12 +0100

    Added customer prio

--- docs/krav/quality.scala
+++ docs/krav/quality.scala
@@ -1,15 +1,22 @@
 Model(
     Section("Usability") has (
-      Quality("UpdateStartview") has 
-      Spec("Updating startview shall be possible with one buttonclick per time")
+      Quality("UpdateStartview") has (
+      Spec("Updating startview shall be possible with one buttonclick per time"),
+		Stakeholder("Customer") has Prio(2)
+		)
       ),
     Section("Efficiency") has (
-      Quality("GPS-position") has 
-      Spec("The system should give a GPS-position correct within a 50 m radius"), 
-      Quality("Bytestid") has 
+      Quality("GPS-position") has (
+      Spec("The system should give a GPS-position correct within a 50 m radius"),
+		Stakeholder("Customer") has Prio(4)
+		),	
+      Quality("Bytestid") has (
       Spec("The product will not have byten of less than 5 min?????"),
-      Quality("requestCapacity") has 
-      Spec("The system needs to be able to process at least 20000 update requests per minute") 
+		Stakeholder("Customer") has Prio(3)
+		),
+      Quality("requestCapacity") has (
+      Spec("The system needs to be able to process at least 20000 update requests per minute")
+		) 
       ),
     Section("Reliability") has (
       Quality("Uptime-Reliability") has 
@@ -18,6 +25,7 @@ Model(
     Section("Security") has (
       Quality("UserIntegrity") has (
         Spec("The system should not provide any statistics that could be used to track a single users travel routines or positions")
+			Stakeholder("Customer") has Prio(5)
         )
     )
   )
