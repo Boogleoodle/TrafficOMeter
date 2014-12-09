3e1f40bea91de5a8ebde0945559f52914e5dbbde
M4tt3 <m4tt3.1337@gmail.com>
2014-12-09 16:02:06 +0100

    modified:   ../../build.sh 	modified:   features.tex 	modified:   functional.scala

--- docs/krav/functional.scala
+++ docs/krav/functional.scala
@@ -3,7 +3,6 @@ Model(
 		Spec("The system should be able to get the current geographical position of the user"),
 		Why("The position is used to be able to calculate the whole trip since the system is supposed to say 'to home in 20 minutes' "),
 		Example("Use native GPS or WiFi-positioning feature of platforms"),
-		Status(ELICITED),
 		Stakeholder("Customer") has Benefit(5)
 	)
 )
