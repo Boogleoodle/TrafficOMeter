a05220d9dcf22c286b9b1bbaee8625b15185e4a4
Naasslan <alexander.nasslander@gmail.com>
2014-12-17 15:19:15 +0100

    Jag är en katt... Samt har fixat referenser

--- docs/krav/functional.scala
+++ docs/krav/functional.scala
@@ -1,9 +1,12 @@
 Model(
-	Feature("GeographicLocation") has (
+	Req("GeographicLocation") has (
 		Spec("The system shall be able to get the current geographical position of the user"),
 		Why("The position is used to be able to calculate the whole trip since the system is supposed to say 'to home in 20 minutes' "),
 		Cost(128),
 		Example("Use native GPS or WiFi-positioning feature of platforms"),
 		Stakeholder("Customer") has Benefit(5)
+	),
+	Req("ExtractingFormat") has (
+		Spec("When extracting statistics from the system it shall be extracted to a semi-colon separated document")
 	)
 )
