1d7a3097099d5cc0bdd0ad0d915286d58a96cd82
M4tt3 <m4tt3.1337@gmail.com>
2014-12-17 19:20:59 +0100

    I have done stuff, such as what I was supposed to do (Kaiser helped!(a lot (really(no kidding (no homo))))) 	modified:   docs/krav/data1.tex 	modified:   docs/krav/data2.tex 	modified:   docs/krav/definitions.tex 	modified:   docs/krav/design.tex 	modified:   docs/krav/features.tex 	modified:   docs/krav/functional.scala 	modified:   docs/krav/functional.tex 	modified:   docs/krav/goalRequirementTracing.tex 	modified:   docs/krav/goals.tex 	modified:   docs/krav/quality.tex 	modified:   docs/krav/task1.tex 	modified:   docs/projectExperiences.tex

--- docs/krav/functional.scala
+++ docs/krav/functional.scala
@@ -7,6 +7,7 @@ Model(
 		Stakeholder("Customer") has Benefit(5)
 	),
 	Req("ExtractingFormat") has (
-		Spec("When extracting statistics from the system it shall be extracted to a semi-colon separated document")
+		Spec("When extracting statistics from the system it shall be extracted to a semi-colon separated document"),
+		Why("Semi-colon separated documents are quite versatile and has support in a wide variety of editors. The format can also easily be converted into spreadsheets.")
 	)
 )
